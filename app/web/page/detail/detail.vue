01e34738112dc111eb9eb7ef7a7ce05f045327af
Chris Cai <caihuijigood@gmail.com>
2018-11-06 22:03:04 +0800

    111

--- app/web/page/detail/detail.vue
+++ app/web/page/detail/detail.vue
@@ -1,7 +1,10 @@
 <template>
     <div id="js-app">
         <div class="operate-bar">
-            <a class="btn btn-default btn-sm btn-outline-primary" href="#" role="button">添加</a>
+            <div style="position: relative;overflow: hidden;pointer:cur">
+                <a class="btn btn-default btn-sm btn-outline-primary">上传离线包 (*.zip) </a>
+                <input class="qui_btn ww_btn ww_fileInputWrap" v-on:change="handleFileChange" href="javascript:;" id="js_upload_file_input" type="file" name="file" value="上传文件"  style="position: absolute;height: 640%;width: 500%;top: 0;right: 0;filter: alpha(opacity=0);padding: 0;margin: 0;opacity: 0;cursor: pointer;">
+            </div>
         </div>
         <table class="table table-hover">
             <thead>
@@ -28,6 +31,7 @@
             </tr>
             </tbody>
         </table>
+
     </div>
 </template>
 
@@ -53,6 +57,28 @@
                         const list = data.data.data;
                         this.list = list;
                     })
+            },
+            async uploadFile (file){
+                return;
+            },
+            handleFileChange (e){
+                let input = e.target;
+                let file = input.files[0];
+
+                let clearFileValue = () =>{
+                    setTimeout(function () {
+                        input.value = '';
+                    }, 500);
+                }
+
+                if (!/\.zip$/gi.test(file.name)) {
+                    clearFileValue();
+                    return ;
+                }
+
+                uploadFile(file)
+
+
             }
         }
     }
